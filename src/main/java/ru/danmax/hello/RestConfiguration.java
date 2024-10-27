06bcf00ef196d3d599830565716363b496e6156c
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-10-27 13:06:38 +0300

    Register resource HelloWorld

--- src/main/java/ru/danmax/hello/RestConfiguration.java
+++ src/main/java/ru/danmax/hello/RestConfiguration.java
@@ -3,10 +3,28 @@ package ru.danmax.hello;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Configures RESTful Web Services for the application.
  */
 @ApplicationPath("resources")
 public class RestConfiguration extends Application {
-    
+    private Set<Object> singletons = new HashSet<Object>();
+    private Set<Class<?>> empty = new HashSet<Class<?>>();
+
+    public RestConfiguration() {
+        singletons.add(new HelloWorldResource());
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return empty;
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
 }
