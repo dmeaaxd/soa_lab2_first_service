9ce28944127403b5f6c06f07742dab1dec180405
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-10-27 16:03:54 +0300

    add DataBase

---
+++ src/main/java/ru/danmax/soa_lab2_first_service/datasource/DataBase.java
@@ -0,0 +1,55 @@
+package ru.danmax.soa_lab2_first_service.datasource;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBase {
+    //  Database credentials
+    static final String URL = "jdbc:postgresql://147.45.190.117:5432/soa_DANMAX";
+    static final String USERNAME = "danmax";
+    static final String PASSWORD = "soaLabs2024";
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() throws SQLException {
+        if (checkConnection()){
+            return connection;
+        } else {
+            restoreConnection();
+            if (checkConnection()){
+                return connection;
+            }
+            else {
+                throw new SQLException("Can't connect to database");
+            }
+        }
+    }
+
+    private static void connect() throws SQLException {
+        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+
+    private static void disconnect() throws SQLException {
+        connection.close();
+    }
+
+    private static boolean checkConnection() {
+        try{
+            return (connection != null && connection.isValid(1) && !connection.isClosed());
+        } catch (SQLException exception){
+            return false;
+        }
+    }
+
+    private static void restoreConnection(){
+        try{
+            if (connection!= null && !connection.isClosed()){
+                disconnect();
+            }
+            connect();
+        } catch (SQLException ignored) {}
+    }
+
+
+}
---
+++ src/main/java/ru/danmax/soa_lab2_first_service/datasource/repositories/DragonRepository.java
@@ -0,0 +1,5 @@
+package ru.danmax.soa_lab2_first_service.datasource.repositories;
+
+public class DragonRepository {
+
+}
