15157e13a4431885c8ee1051728a2017a7046ecf
Maksim Ushakov <dmeaaxd@gmail.com>
2024-10-27 17:19:23 +0300

    add repository

--- src/main/java/ru/danmax/soa_lab2_first_service/datasource/repositories/additional/AdditionalMethods.java
+++ src/main/java/ru/danmax/soa_lab2_first_service/datasource/repositories/additional/AdditionalMethods.java
@@ -2,6 +2,8 @@ package ru.danmax.soa_lab2_first_service.datasource.repositories.additional;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AdditionalMethods {
 
@@ -28,6 +30,30 @@ public class AdditionalMethods {
     }
 
     public static void setFilterParameters(PreparedStatement stmt, String filter) throws SQLException {
+        String regex = "\\((\\w+)\\s+(eq|ne|lt|le|gt|ge)\\s+([^()]+)\\)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(filter);
 
+        int parameterIndex = 1;
+
+        while (matcher.find()) {
+            String field = matcher.group(1);
+            String operator = matcher.group(2);
+            String value = matcher.group(3).trim();
+
+            if ("id".equals(field)) {
+                stmt.setLong(parameterIndex++, Long.parseLong(value));
+            } else if ("name".equals(field)) {
+                stmt.setString(parameterIndex++, value);
+            } else if ("age".equals(field)) {
+                stmt.setInt(parameterIndex++, Integer.parseInt(value));
+            } else if ("color".equals(field)) {
+                stmt.setString(parameterIndex++, value);
+            } else if ("dragon_type".equals(field)) {
+                stmt.setString(parameterIndex++, value);
+            } else if ("character".equals(field)) {
+                stmt.setString(parameterIndex++, value);
+            }
+        }
     }
 }
