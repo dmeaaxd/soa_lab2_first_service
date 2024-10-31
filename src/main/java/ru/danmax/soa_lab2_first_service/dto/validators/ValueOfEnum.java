0691edc0db89f93b16de51d2647fe876f5744101
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-10-31 22:51:26 +0300

    update value of enum validator

---
+++ src/main/java/ru/danmax/soa_lab2_first_service/dto/validators/ValueOfEnum.java
@@ -0,0 +1,22 @@
+package ru.danmax.soa_lab2_first_service.dto.validators;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = ValueOfEnumValidator.class)
+public @interface ValueOfEnum {
+    Class<? extends Enum<?>> enumClass();
+    String message() default "Значение параметра должно быть одним из enum {enumClass}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
