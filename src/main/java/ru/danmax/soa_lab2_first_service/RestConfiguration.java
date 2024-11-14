211b8ea9767278440b32ecda8953d4b1fc15b411
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-11-14 20:04:05 +0300

    update main path

--- src/main/java/ru/danmax/soa_lab2_first_service/RestConfiguration.java
+++ src/main/java/ru/danmax/soa_lab2_first_service/RestConfiguration.java
@@ -2,17 +2,15 @@ package ru.danmax.soa_lab2_first_service;
 
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
-import ru.danmax.soa_lab2_first_service.datasource.DataBase;
 import ru.danmax.soa_lab2_first_service.resources.DragonResource;
 
-import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Configures RESTful Web Services for the application.
  */
-@ApplicationPath("resources")
+@ApplicationPath("")
 public class RestConfiguration extends Application {
     private Set<Object> singletons = new HashSet<Object>();
     private Set<Class<?>> empty = new HashSet<Class<?>>();
