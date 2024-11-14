767f71e8ba6cb496b52ca38a197f57d6f07d9478
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-11-14 21:08:43 +0300

    add page and size to get dragons

--- src/main/java/ru/danmax/soa_lab2_first_service/Test.java
+++ src/main/java/ru/danmax/soa_lab2_first_service/Test.java
@@ -1,12 +1,16 @@
 package ru.danmax.soa_lab2_first_service;
 
-import ru.danmax.soa_lab2_first_service.datasource.repositories.DragonRepository;
+import jakarta.ws.rs.core.Response;
+import ru.danmax.soa_lab2_first_service.resources.DragonResource;
 
 import java.sql.SQLException;
 
 public class Test {
-    public static void main(String[] args) throws SQLException {
-        String filters = "(id eq 10) and (name eq Dragon)";
-        System.out.println(DragonRepository.findAll(null, null));
+    public static void main(String[] args) {
+        DragonResource dragonResource = new DragonResource();
+
+        // Тест 1
+        Response response = dragonResource.getDragons(null, null, null, null);
+        System.out.println(response);
     }
 }
