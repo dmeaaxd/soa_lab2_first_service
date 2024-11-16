8098a64cd29a8da5b6437ff27cde793ffb89e210
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-11-16 13:50:21 +0300

    add cors headers

--- src/main/java/ru/danmax/soa_lab2_first_service/CorsFilter.java
+++ src/main/java/ru/danmax/soa_lab2_first_service/CorsFilter.java
@@ -4,26 +4,29 @@ import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.container.PreMatching;
+import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.ext.Provider;
-
-import java.io.IOException;
 
 @Provider
 @PreMatching
 public class CorsFilter implements ContainerResponseFilter {
 
-    @Override
-    public void filter(ContainerRequestContext requestContext,
-                       ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().putSingle(
-                "Access-Control-Allow-Origin", "*");
-        responseContext.getHeaders().putSingle(
-                "Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().putSingle(
-                "Access-Control-Allow-Headers",
-                "origin, content-type, accept, authorization");
-        responseContext.getHeaders().putSingle(
-                "Access-Control-Allow-Methods",
-                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        String origin = requestContext.getHeaderString("Origin");
+        if ((origin != null)
+                && origin.startsWith("http://localhost")) {
+            allowExceptionCors(requestContext, responseContext, origin);
+        }
+    }
+
+    private void allowExceptionCors(ContainerRequestContext requestContext, ContainerResponseContext responseContext, String origin) {
+        String methodHeader = requestContext.getHeaderString("Access-Control-Request-Method");
+        String requestHeaders = requestContext.getHeaderString("Access-Control-Request-Headers");
+
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+        headers.putSingle("Access-Control-Allow-Origin", origin);
+        headers.putSingle("Access-Control-Allow-Credentials", "true");
+        headers.putSingle("Access-Control-Allow-Methods", methodHeader);
+        headers.putSingle("Access-Control-Allow-Headers", "x-requested-with," + (requestHeaders == null ? "" : requestHeaders));
     }
 }
