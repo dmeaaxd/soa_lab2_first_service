db4bab71bc1f46fc07b20f247bc5082012fb2bd2
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-11-16 13:46:12 +0300

    add cors headers

--- src/main/java/ru/danmax/soa_lab2_first_service/CorsFilter.java
+++ src/main/java/ru/danmax/soa_lab2_first_service/CorsFilter.java
@@ -15,14 +15,14 @@ public class CorsFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add(
+        responseContext.getHeaders().putSingle(
                 "Access-Control-Allow-Origin", "*");
-        responseContext.getHeaders().add(
+        responseContext.getHeaders().putSingle(
                 "Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add(
+        responseContext.getHeaders().putSingle(
                 "Access-Control-Allow-Headers",
                 "origin, content-type, accept, authorization");
-        responseContext.getHeaders().add(
+        responseContext.getHeaders().putSingle(
                 "Access-Control-Allow-Methods",
                 "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
