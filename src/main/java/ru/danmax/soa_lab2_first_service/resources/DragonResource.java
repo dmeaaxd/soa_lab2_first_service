2430dfc6acc9ef93ecf8c94903251f920fa752b7
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-11-16 13:50:44 +0300

    add cors headers

--- src/main/java/ru/danmax/soa_lab2_first_service/resources/DragonResource.java
+++ src/main/java/ru/danmax/soa_lab2_first_service/resources/DragonResource.java
@@ -248,12 +248,13 @@ public class DragonResource {
     }
 
     private Response.ResponseBuilder addCorsHeaders(Response.ResponseBuilder responseBuilder){
-        return responseBuilder
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Credentials", "true")
-                .header("Access-Control-Allow-Headers",
-                        "origin, content-type, accept, authorization")
-                .header("Access-Control-Allow-Methods",
-                        "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+//        return responseBuilder
+//                .header("Access-Control-Allow-Origin", "*")
+//                .header("Access-Control-Allow-Credentials", "true")
+//                .header("Access-Control-Allow-Headers",
+//                        "origin, content-type, accept, authorization")
+//                .header("Access-Control-Allow-Methods",
+//                        "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        return responseBuilder;
     }
 }
